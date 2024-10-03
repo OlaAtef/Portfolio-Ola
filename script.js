1072dd9e5bed9bf09eaa3f0705b48deec07d8d86
Ola <ola736492@gmail.com>
2024-10-03 04:06:57 +0300

    ....

--- script.js
+++ script.js
@@ -24,17 +24,16 @@ function opentab( tabname ){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
-// Menu Icon........
-function openmenu(){
-    sidemenu.style.right = "0";
-}
-function closemenu(){
-    sidemenu.style.right = "-200px";
+
+// show Menu Bar........
+document.querySelector('#menu-icon').onclick = () =>{
+    sidemenu.classList.toggle('active');
 }
 
-window.onscroll = ()=>{
-    sidemenu.style.right = "-200px";
+document.querySelector('#close-menu').onclick = () =>{
+    sidemenu.classList.remove('active');
 }
+
 // Switch Mode The Website..........
 const toggle_mode = document.querySelector('#toggle-mode');
 toggle_mode.addEventListener('click', (e) =>{
@@ -44,12 +43,12 @@ if(current_mode === 'dark'){
     document.documentElement.style.setProperty('--first-color', '#080808');
     document.documentElement.style.setProperty('--third-color', '#fff');
 const current_mode = document.body.dataset.mode = 'light';
-e.target.innerHTML = 'Light Mode';
+// e.target.innerHTML = 'Light Mode';
 } else if (current_mode === 'light'){
     document.documentElement.style.setProperty('--first-color', '#fff');
     document.documentElement.style.setProperty('--third-color', '#080808');
 const current_mode = document.body.dataset.mode = 'dark';
-e.target.innerHTML = 'Dark Mode';
+// e.target.innerHTML = 'Dark Mode';
 }
 });
 
@@ -70,3 +69,18 @@ function ActiveMenu(){
 ActiveMenu();
 window.addEventListener('scroll', ActiveMenu)
 
+
+window.onscroll = ()=>{
+    ScroollHeader();
+}
+
+let Header = document.getElementById('fixed-nav');
+let x = 0;
+
+function ScroollHeader(){
+if(window.pageYOffset > x){
+    Header.classList.add('FixedHeader');
+}else{
+    Header.classList.remove('FixedHeader');
+}
+}
